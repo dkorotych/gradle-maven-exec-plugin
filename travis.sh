ae7998da17d32af6b68306e3ef5b793166258de7
Dmitry Korotych <dkorotych@gmail.com>
2016-11-27 01:40:43 +0300

    Downgrade target version

---
+++ travis.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+./gradlew check
+if [ -z "$ONLY_CHECK" ]; then
+    ./gradlew cobertura
+    ./gradlew sonarqube -Dsonar.host.url=$sonar_host -Dsonar.login=$sonar_login
+    bash <`curl -s https://codecov.io/bash`
+fi
