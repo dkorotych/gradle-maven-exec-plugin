b35a31667c207ef914819b7bc755c3c70c6d9037
Dmitry Korotych <dkorotych@gmail.com>
2016-11-30 02:48:23 +0300

    Fixed documentation builder

--- travis/deploy-gh-pages.sh
+++ travis/deploy-gh-pages.sh
@@ -3,11 +3,11 @@
 set -e
 
 ./gradlew groovydoc
-cd build/docs
+cd build/docs/groovydoc
 
 git init
 git config user.name "Travis"
 git config user.email "travis@travis-ci.org"
 git add .
 git commit -m "Deployed to Github Pages"
-git push --force --quiet "https://${GH_TOKEN}@${TRAVIS_REPO_SLUG}" master:gh-pages
+git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}" master:gh-pages > /dev/null 2>&1
