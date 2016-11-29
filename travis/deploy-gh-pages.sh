d56c3ecc2dc74d45ee6197b1f6106e3ac054a4a6
Dmitry Korotych <dkorotych@gmail.com>
2016-11-29 20:05:06 +0300

    Added documentation

---
+++ travis/deploy-gh-pages.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+./gradlew groovydoc
+cd build/docs
+
+git init
+git config user.name "Travis"
+git config user.email "travis@travis-ci.org"
+git add .
+git commit -m "Deployed to Github Pages"
+git push --force --quiet "https://${GH_TOKEN}@${TRAVIS_REPO_SLUG}" master:gh-pages > /dev/null 2>&1
