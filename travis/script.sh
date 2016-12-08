306f544f08cc226d7ef9112fa5c676a9fbd0fe94
Dmitry Korotych <dkorotych@gmail.com>
2016-12-09 01:30:03 +0300

    Fix for correct execute sonar analyzer

--- travis/script.sh
+++ travis/script.sh
@@ -6,6 +6,6 @@ set -e
 if [ -z "$ONLY_CHECK" ]; then
     ./gradlew cobertura
     ~/jpm/bin/codacy-coverage-reporter --language Java --coverageReport build/reports/cobertura/coverage.xml --projectToken "$codacy_project_token"
-    ./gradlew clean sonarqube -Dsonar.host.url=$sonar_host -Dsonar.login=$sonar_login
+    ./gradlew sonarqube -Dsonar.host.url=$sonar_host -Dsonar.login=$sonar_login
     ./codecov.sh
 fi
