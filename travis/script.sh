77720a5a1b10cd183d7bf6e8a63433a2bfecd765
Dmitry Korotych <dkorotych@gmail.com>
2018-02-27 00:32:29 +0300

    Tests for validate plugin in modern Gradle versions

--- travis/script.sh
+++ travis/script.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-./gradlew check realUseCaseTest
+./gradlew install check realUseCaseTest
 if [ -z "$ONLY_CHECK" ]; then
     ./gradlew cobertura
     # Send information to https://codacy.com
