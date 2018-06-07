cb47405204a2ef54ab01a91d1813c9abec83d2f1
Dmitry Korotych <dkorotych@gmail.com>
2018-06-07 21:17:33 +0300

    Cache coverage jar and redownload it if needed

--- travis/coverage.sh
+++ travis/coverage.sh
@@ -2,16 +2,15 @@
 
 set -e
 
-readonly codacy="codacy-coverage-reporter-assembly-latest.jar"
 readonly url=$(curl -s https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r .assets[0].browser_download_url)
 
-wget -q -O ${codacy} "$url"
+wget -q -N -c -O "$CODACY_JAR" "$url"
 (curl -s https://codecov.io/bash) > codecov.sh
 chmod +x codecov.sh
 
 ./gradlew cobertura
 # Send information to https://codacy.com
-java -cp "$codacy" com.codacy.CodacyCoverageReporter --language Java --coverageReport build/reports/cobertura/coverage.xml --projectToken "$codacy_project_token"
+java -cp "$CODACY_JAR" com.codacy.CodacyCoverageReporter --language Java --coverageReport build/reports/cobertura/coverage.xml --projectToken "$codacy_project_token"
 # Send information to https://sonarqube.com
 #    ./gradlew sonarqube -Dsonar.host.url=$sonar_host -Dsonar.login=$sonar_login
 ./codecov.sh
