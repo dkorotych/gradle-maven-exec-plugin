50b5099dc7aaba4fc5ea7208022a50a84436070f
Dmitry Korotych <dkorotych@gmail.com>
2018-06-07 19:09:34 +0300

    Move coverage code to script

--- travis/coverage.sh
+++ travis/coverage.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+wget -O ~/codacy-coverage-reporter-assembly-latest.jar "$(curl https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r .assets[0].browser_download_url)"
+(curl -s https://codecov.io/bash) > codecov.sh
+chmod +x codecov.sh
+
 ./gradlew cobertura
 # Send information to https://codacy.com
 java -cp ~/codacy-coverage-reporter-assembly-latest.jar com.codacy.CodacyCoverageReporter --language Java --coverageReport build/reports/cobertura/coverage.xml --projectToken "$codacy_project_token"
