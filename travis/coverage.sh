4d02a81f6c542695a821d8ff63568ed25027b00a
Dmitry Korotych <dkorotych@gmail.com>
2018-06-07 19:30:19 +0300

    Move coverage code to script

--- travis/coverage.sh
+++ travis/coverage.sh
@@ -2,13 +2,16 @@
 
 set -e
 
-wget -O ~/codacy-coverage-reporter-assembly-latest.jar "$(curl https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r .assets[0].browser_download_url)"
+readonly codacy="~/codacy-coverage-reporter-assembly-latest.jar"
+readonly url=$(curl https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r .assets[0].browser_download_url)
+
+wget -O ${codacy} "$url"
 (curl -s https://codecov.io/bash) > codecov.sh
 chmod +x codecov.sh
 
 ./gradlew cobertura
 # Send information to https://codacy.com
-java -cp ~/codacy-coverage-reporter-assembly-latest.jar com.codacy.CodacyCoverageReporter --language Java --coverageReport build/reports/cobertura/coverage.xml --projectToken "$codacy_project_token"
+java -cp "$codacy" com.codacy.CodacyCoverageReporter --language Java --coverageReport build/reports/cobertura/coverage.xml --projectToken "$codacy_project_token"
 # Send information to https://sonarqube.com
 #    ./gradlew sonarqube -Dsonar.host.url=$sonar_host -Dsonar.login=$sonar_login
 ./codecov.sh
