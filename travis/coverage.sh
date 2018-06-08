350f6ccc66d7491347138e3c260719f252ecbb86
Dmitry Korotych <dkorotych@gmail.com>
2018-06-08 10:30:25 +0300

    Not use Codacy plugin in Java 7

--- travis/coverage.sh
+++ travis/coverage.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Send information to https://codacy.com
-./gradlew jacocoTestReportCodacyUpload --codacy-token="$codacy_project_token"
+./gradlew test jacocoTestReportCodacyUpload --codacy-token="$codacy_project_token"
 # Send information to https://sonarqube.com
 #    ./gradlew sonarqube -Dsonar.host.url=$sonar_host -Dsonar.login=$sonar_login
 (curl -s https://codecov.io/bash) | bash
