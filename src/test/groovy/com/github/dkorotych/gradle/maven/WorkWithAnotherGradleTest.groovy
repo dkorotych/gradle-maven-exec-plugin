e4aea8734e73c8eadd9159a75b170b43a882d86e
Dmitry Korotych <dkorotych@gmail.com>
2018-11-30 19:29:05 +0300

    Execute tests in Gradle 5.0

--- src/test/groovy/com/github/dkorotych/gradle/maven/WorkWithAnotherGradleTest.groovy
+++ src/test/groovy/com/github/dkorotych/gradle/maven/WorkWithAnotherGradleTest.groovy
@@ -67,7 +67,7 @@ class WorkWithAnotherGradleTest extends Specification {
         cleanupProject()
 
         where:
-        version << ['3.0', '4.0', '4.5.1', '4.6', '4.10.2']
+        version << ['3.0', '4.0', '4.5.1', '4.6', '4.10.2', '5.0']
     }
 
     private Wrapper wrapper(String version) {
