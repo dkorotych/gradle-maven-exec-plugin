27fa76479908640dc4f6ee327045cec0d10f76eb
Dmitry Korotych <dkorotych@gmail.com>
2022-08-02 16:40:23 +0300

    Try to run validation on the matrix of OS and matrix of Java versions

--- buildSrc/src/main/java/com/github/dkorotych/gradle/maven/exec/test/AbstractGenerationTask.java
+++ buildSrc/src/main/java/com/github/dkorotych/gradle/maven/exec/test/AbstractGenerationTask.java
@@ -3,11 +3,9 @@ package com.github.dkorotych.gradle.maven.exec.test;
 import org.gradle.api.GradleException;
 import org.gradle.api.Project;
 import org.gradle.api.tasks.TaskAction;
-import org.gradle.process.ExecResult;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.util.*;
@@ -41,31 +39,21 @@ abstract class AbstractGenerationTask extends MavenDependentTask {
     public void generate() {
         final Project project = getProject();
         final ByteArrayOutputStream errorStream = new ByteArrayOutputStream();
-        ExecResult result = null;
         try (OutputStream outputStream = Files.newOutputStream(outputFile.toPath())) {
-            result = project.exec(execSpec -> {
+            project.exec(execSpec -> {
                 execSpec.executable(getMavenExecutable())
                         .workingDir(getMavenHome());
                 execSpec.setStandardOutput(outputStream);
                 execSpec.setErrorOutput(errorStream);
                 execSpec.setArgs(options);
-            });
+            }).assertNormalExitValue();
         } catch (Exception e) {
-            System.out.println("e = " + e);
-            System.out.println("errorStream = " + errorStream.toString());
             String description = buildCauseMessagesWithoutLast(e)
                     .collect(Collectors.joining(System.getProperty("line.separator")));
             if (description.trim().isEmpty()) {
                 description = e.getMessage();
             }
             throw new GradleException(description, e);
-        }
-        if (result != null && result.getExitValue() != 0) {
-            try {
-                Files.copy(outputFile.toPath(), System.out);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
         }
     }
 
