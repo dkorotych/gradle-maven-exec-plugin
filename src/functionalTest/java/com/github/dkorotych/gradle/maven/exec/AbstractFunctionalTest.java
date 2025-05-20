4222dffb20cdf26eead419fbc2db42725ced8dc9
Dmitry Korotych <dkorotych@gmail.com>
2025-05-20 23:04:20 +0300

    update dependencies

--- src/functionalTest/java/com/github/dkorotych/gradle/maven/exec/AbstractFunctionalTest.java
+++ src/functionalTest/java/com/github/dkorotych/gradle/maven/exec/AbstractFunctionalTest.java
@@ -43,7 +43,8 @@ public abstract class AbstractFunctionalTest {
             "8.10.2",
             "8.11.1",
             "8.12.1",
-            "8.13"
+            "8.13",
+            "8.14"
     );
 
     private static final GradleVersion MINIMAL_SUPPORTED_GRADLE_VERSION = GradleVersion.version("8.5");
